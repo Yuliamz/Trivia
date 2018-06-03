c9b7685ff7bbb530993df9b1f7998ed2abd7f2a1
Julian David <caprijuda@gmail.com>
2018-06-03 14:23:43 -0500

    Initial application generated by JHipster-4.14.4

---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('triviaApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('triviaApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
